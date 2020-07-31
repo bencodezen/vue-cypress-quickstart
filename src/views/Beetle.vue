0885da5cce12506468f14a0e863764f81dc8fa26
Ben Hong <ben@bencodezen.io>
2020-07-31 15:21:44 -0400

    test: route2 tests

---
+++ src/views/Beetle.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios'
+
+export default {
+  methods: {
+    sendBeetleRequest() {
+      axios.post('/beetles-2').then(response => console.log(response))
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <h1>Beetles</h1>
+    <input type="text" id="search" />
+    <div id="beetle-results"></div>
+    <button id="sendRequest" @click="sendBeetleRequest">Send Request</button>
+  </main>
+</template>
+
+<style></style>
